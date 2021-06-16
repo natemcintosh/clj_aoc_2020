bdc19c39a18a62667c1e724c83e0466818b8e13c
Nathan McIntosh <natemcintosh@gmail.com>
2021-06-16 12:20:06 -0400

    working on day 3

--- test/all_test.clj
+++ test/all_test.clj
@@ -1,7 +1,8 @@
 (ns all-test
   (:require [clojure.test :as t]
             [day01]
-            [day02]))
+            [day02]
+            [day03]))
 
 (t/deftest day1
   (let [d [1721 979 366 299 675 1456]]
@@ -14,3 +15,42 @@
            {:lbound 2 :ubound 9 :key \c :pw "ccccccccc"}]]
     (t/is (= 2 (day02/part1 d)))
     (t/is (= 1 (day02/part2 d)))))
+
+(t/deftest day3
+  (let [d [{:ecl "gry"
+            :pid 860033327
+            :eyr 2020
+            :hcl "#fffffd"
+            :byr 1937
+            :iyr 2017
+            :cid 147
+            :hgt "183cm"}
+           {:iyr 2013
+            :ecl "amb"
+            :cid 350
+            :eyr 2023
+            :pid 28048884
+            :hcl "#cfa07d"
+            :byr 1929}
+           {:hcl "#ae17e1"
+            :iyr 2013
+            :eyr 2024
+            :ecl "brn"
+            :pid 760753108
+            :byr 1931
+            :hgt "179cm"}
+           {:hcl "#cfa07d"
+            :eyr 2025
+            :pid 166559648
+            :iyr 2011
+            :ecl "brn"
+            :hgt "59in"}]]
+    (t/is (= 2 (day03/part1 d)))))
+
+
+
+
+
+
+
+
